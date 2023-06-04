ef708ee632b699ff3a471394fb54b5ea3ac88e46
Manish Srivastav <84321476+Mani5746@users.noreply.github.com>
2023-06-04 21:44:17 +0530

    Create Array : Next Permutation.cpp

---
+++ Array : Next Permutation.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+    int i,j;
+
+    for(i=n-2;i>=0;i--)
+    {
+        if(permutation[i]<permutation[i+1])
+        {
+            break;
+        }
+    }
+
+    if(i<0) 
+    {
+        reverse(permutation.begin(),permutation.end());
+    }
+
+    else
+    {
+        for(j=n-1;j>=0;j--)
+        {
+            if(permutation[j]>permutation[i])
+            {
+                swap(permutation[j],permutation[i]);
+                break;
+            }
+        }
+
+        reverse(permutation.begin()+i+1,permutation.end());
+    }
+
+    return permutation;
+}
