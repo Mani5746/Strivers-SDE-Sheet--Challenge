7f6a3b8222fa0d3b074644564ee34d6a2631a630
Manish Srivastav <84321476+Mani5746@users.noreply.github.com>
2023-06-06 22:29:23 +0530

    Create Array: FindDupicate Elements.cpp

---
+++ Array: FindDupicate Elements.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n)
+{
+	// Write your code here.
+	sort(arr.begin(),arr.end());
+	for(int i=1;i<n;i++)
+	{
+		if(arr[i-1]==arr[i])  return arr[i-1];
+	}
+
+	return 0;
+}
